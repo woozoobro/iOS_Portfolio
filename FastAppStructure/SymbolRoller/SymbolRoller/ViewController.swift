c92d7fb72f21eeef7dfeba28e80b7736b0eb0a10
woozoobro <woojusm@gmail.com>
2022-12-24 19:09:36 +0900

    SymbolRoller

---
+++ FastAppStructure/SymbolRoller/SymbolRoller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SymbolRoller
+//
+//  Created by 우주형 on 2022/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
