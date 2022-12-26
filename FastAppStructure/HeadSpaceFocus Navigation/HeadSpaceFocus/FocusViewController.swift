9de96bdb513a2a1d9e9b64ccdd47118f31598283
woozoobro <woojusm@gmail.com>
2022-12-27 01:31:14 +0900

    Navigation

---
+++ FastAppStructure/HeadSpaceFocus Navigation/HeadSpaceFocus/FocusViewController.swift
@@ -0,0 +1,73 @@
+//
+//  FocusViewController.swift
+//  HeadSpaceFocus
+//
+//  Created by 우주형 on 2022/12/26.
+//
+
+import UIKit
+
+class FocusViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    var curated: Bool = false
+    
+    var items: [Focus] = Focus.list
+    
+    typealias Item = Focus
+    enum Section { case main }
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FocusCell", for: indexPath) as? FocusCell else { return nil }
+            cell.configure(item)
+            return cell
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items, toSection: .main)
+        dataSource.apply(snapshot)
+        
+        collectionView.collectionViewLayout = layout()
+        
+        refreshButton.layer.cornerRadius = 14
+        updateButtonTitle()
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+    func updateButtonTitle() {
+        let title = curated ? "See All" : "See Recommendation"
+        refreshButton.setTitle(title, for: .normal)
+    }
+    
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+        curated.toggle()
+        
+        self.items = curated ? Focus.recommendations : Focus.list
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items, toSection: .main)
+        dataSource.apply(snapshot)
+        
+        updateButtonTitle()
+    }
+    
+}
+
