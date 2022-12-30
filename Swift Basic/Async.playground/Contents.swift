3e187f0c6c6703ff926045d346081dfc6606a144
woozoobro <woojusm@gmail.com>
2022-12-30 22:29:09 +0900

    sync Async

---
+++ Swift Basic/Async.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+
+
+//print("Hey Support")
+//sleep(3)
+//print("Water!")
+
+ 
+
+
+
+
+
+
+
+print("Hey Support")
+DispatchQueue.main.async {
+    sleep(3)
+    print("No Water")
+}
+print("Water!")
