a23ff8b1cd8ff4b10ab04d8f2c3b2084bf45c805
woozoobro <woojusm@gmail.com>
2022-12-30 18:01:28 +0900

    network

--- FastAppStructure/Network in iOS.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
+++ FastAppStructure/Network in iOS.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
@@ -1,7 +1,64 @@
-//: [Previous](@previous)
-
 import Foundation
+import Combine
 
-var greeting = "Hello, playground"
+enum NetworkError: Error {
+    case invalidRequest
+    case transportError(Error)
+    case responseError(statusCode: Int)
+    case noData
+    case decodingError(Error)
+}
 
-//: [Next](@next)
+struct GithubProfile: Codable {
+    let login: String
+    let avatarURL: String
+    let htmlURL: String
+    let followers: Int
+    let following: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case login
+        case avatarURL = "avatar_url"
+        case htmlURL = "html_url"
+        case followers
+        case following
+    }
+}
+
+final class NetworkService {
+    let session: URLSession
+    
+    init(configuration: URLSessionConfiguration) {
+        session = URLSession(configuration: configuration)
+    }
+    
+    func fetchProfile(userName: String) -> AnyPublisher<GithubProfile, Error> {
+        let url = URL(string: "https://api.github.com/users/\(userName)")!
+        
+        let publisher = session
+            .dataTaskPublisher(for: url)
+        // 서버에서 받은 response 확인
+            .tryMap { result -> Data in
+                guard let httpResponse = result.response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+                    let response = result.response as? HTTPURLResponse
+                    let statusCode = response?.statusCode ?? -1
+                    throw NetworkError.responseError(statusCode: statusCode)
+                }
+                return result.data
+            } // 받은 Data 디코딩 잘하기
+            .decode(type: GithubProfile.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+        return publisher
+    }
+}
+
+let networkService = NetworkService(configuration: .default)
+
+let subscription = networkService.fetchProfile(userName: "woozoobro")
+    .receive(on: RunLoop.main)
+    .print()
+    .sink { completion in
+        print("completion: \(completion)")
+    } receiveValue: { profile in
+        print("profile: \(profile)")
+    }
