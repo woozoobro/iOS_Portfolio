94063e65a3bfe1c7d6579419ff730976024131e5
woozoobro <woojusm@gmail.com>
2023-01-05 17:10:21 +0900

    Timer

---
+++ PersonalProject/TimerCheck/TimerCheck/Model/TimePassed.swift
@@ -0,0 +1,24 @@
+//
+//  TimePassed.swift
+//  TimerCheck
+//
+//  Created by 우주형 on 2023/01/05.
+//
+
+import Foundation
+
+struct TimePassed {
+    var secondsPassed = 0
+    var minutesPassed = 0
+    var houresPassed = 0
+    
+    func currentTimetoString(sec: Int, min: Int, hour: Int) -> String {
+        if min > 0 {
+            return "\(minutesPassed)분 \(secondsPassed) 초"
+        } else if hour > 0 {
+            return "\(houresPassed)시간 \(minutesPassed)분 \(secondsPassed) 초"
+        } else {
+            return "\(secondsPassed) 초"
+        }
+    }
+}
--- PersonalProject/TimerCheck/TimerCheck/ViewController.swift
+++ PersonalProject/TimerCheck/TimerCheck/ViewController.swift
@@ -11,8 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var timeLabel: UILabel!
     
-    var secondsPassed = 0
     var timer: Timer?
+    var timePassed = TimePassed()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +22,22 @@ class ViewController: UIViewController {
     @IBAction func startButtonPressed(_ sender: UIButton) {
         timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { _ in
-            self.secondsPassed += 1
+            self.timePassed.secondsPassed += 1
+            if self.timePassed.secondsPassed == 60 {
+                self.timePassed.secondsPassed = 0
+                self.timePassed.minutesPassed += 1
+                
+                if self.timePassed.minutesPassed == 60 {
+                    self.timePassed.minutesPassed = 0
+                    self.timePassed.houresPassed += 1
+                }
+            }
             DispatchQueue.main.async {
-                self.timeLabel.text = "\(self.secondsPassed) 초"
+                self.timeLabel.text = self.timePassed.currentTimetoString(sec: self.timePassed.secondsPassed, min: self.timePassed.minutesPassed, hour: self.timePassed.houresPassed)
             }
         })
     }
+   
     
 }
 
