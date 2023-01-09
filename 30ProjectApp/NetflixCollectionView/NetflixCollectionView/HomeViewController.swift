5430546a68f722d4765fbf4844a2f7808a40519b
woozoobro <woojusm@gmail.com>
2023-01-10 06:05:51 +0900

    NetflixCollectionView

---
+++ 30ProjectApp/NetflixCollectionView/NetflixCollectionView/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  NetflixCollectionView
+//
+//  Created by 우주형 on 2023/01/10.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //네비게이션 설정
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.hidesBarsOnSwipe = true
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "netflix_icon"), style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
+        
+    }
+    
+    
+    
+    
+    
+}
