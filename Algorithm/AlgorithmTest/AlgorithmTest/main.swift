1268de0bdb958e1ba88241272e9cdbc56dab370c
woozoobro <woojusm@gmail.com>
2023-01-11 08:54:31 +0900

    command line

---
+++ Algorithm/AlgorithmTest/AlgorithmTest/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  AlgorithmTest
+//
+//  Created by 우주형 on 2023/01/11.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ PersonalProject/MyCreditManager/MyCreditManager/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  MyCreditManager
+//
+//  Created by 우주형 on 2023/01/11.
+//
+
+import Foundation
+
