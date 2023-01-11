004577c773b386312d8132f9b9b05cc3da591735
woozoobro <woojusm@gmail.com>
2023-01-12 07:06:52 +0900

    Timer

--- PersonalProject/TimerCheck/TimerCheck/TimeListViewController.swift
+++ PersonalProject/TimerCheck/TimerCheck/TimeListViewController.swift
@@ -9,50 +9,61 @@ import UIKit
 import CoreData
 
 class TimeListViewController: UIViewController {
-
-//    var list = [TimeModel]()
+    
+    var list = [TimeModel]()
     var timeItem = [TimeItem]()
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     enum Section { case main }
-    typealias Item = TimeItem
+    typealias Item = TimeModel
     var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let timeVC = tabBarController?.viewControllers?[0] as? TimeViewController else { return }
-        timeVC.delegate = self
         collectionView.delegate = self
         
+        loadItems()
+        guard let timeVC = tabBarController?.viewControllers?[0] as? TimeViewController else { return }
+        timeVC.delegate = self
         dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TimeListCell", for: indexPath) as? TimeListCell else { return UICollectionViewCell() }
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TimeListCell", for: indexPath) as? TimeListCell else { return nil }
             cell.configure(item)
+            
+            
             return cell
         })
         
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(timeItem)
-        dataSource.apply(snapshot)
+        updateList()
         
-        collectionView.collectionViewLayout = layout()
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        
+        collectionView.collectionViewLayout = layout
     }
     
-    private func layout() -> UICollectionViewCompositionalLayout {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(100))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(100))
-        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
-        let section = NSCollectionLayoutSection(group: group)
-        return UICollectionViewCompositionalLayout(section: section)
+    private func updateList() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(list)
+        dataSource.apply(snapshot)
     }
+    
+//    private func layout() -> UICollectionViewCompositionalLayout {
+//        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(100))
+//        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(100))
+//        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+//        let section = NSCollectionLayoutSection(group: group)
+//        return UICollectionViewCompositionalLayout(section: section)
+//    }
     
     private func loadItems() {
         let request: NSFetchRequest<TimeItem> = TimeItem.fetchRequest()
         do {
             timeItem = try context.fetch(request)
+            list = timeItem.map { TimeModel(seconds: Int($0.seconds ?? "0") ?? 0, minutes: Int($0.minutes ?? "0") ?? 0, hours: Int($0.hours ?? "0") ?? 0, date: $0.date!) }
         } catch {
             print("Error fetching data from context \(error)")
         }
@@ -60,14 +71,12 @@ class TimeListViewController: UIViewController {
 }
 //MARK: - TimeVC Delegate
 extension TimeListViewController: TimeViewControllerDelegate {
-    func didSavedTime(data: TimeModel) {        
+    func didSavedTime(data: TimeModel) {
         list.append(data)
-        var snapshot = dataSource.snapshot()
-        snapshot.appendItems([data], toSection: .main)
-        dataSource.apply(snapshot)
+        updateList()
     }
 }
-//MARK: -
+
 extension TimeListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print(list[indexPath.item])
