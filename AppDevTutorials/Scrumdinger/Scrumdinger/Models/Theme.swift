dc706c34c912586f3bdf0e0267876818827080f5
woozoobro <woojusm@gmail.com>
2023-01-13 01:11:00 +0900

    Scrumdinger

---
+++ AppDevTutorials/Scrumdinger/Scrumdinger/Models/Theme.swift
@@ -0,0 +1,37 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by 우주형 on 2023/01/12.
+//
+
+import SwiftUI
+
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seaform
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seaform, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+    var mainColor: Color {
+        Color(rawValue)
+    }
+}
