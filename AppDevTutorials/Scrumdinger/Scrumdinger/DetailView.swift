a0a9780ce7a043970d66808dc1ab4216b0120202
woozoobro <woojusm@gmail.com>
2023-01-13 17:50:05 +0900

    Scrumdinger

--- AppDevTutorials/Scrumdinger/Scrumdinger/DetailView.swift
+++ AppDevTutorials/Scrumdinger/Scrumdinger/DetailView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
+    
+    @State private var isPresentingEditView = false
+    
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -39,7 +42,31 @@ struct DetailView: View {
                     Label(attendee.name, systemImage: "person")
                 }
             }
-        }.navigationTitle(scrum.title)
+        }
+        .navigationTitle(scrum.title)
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationView {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
