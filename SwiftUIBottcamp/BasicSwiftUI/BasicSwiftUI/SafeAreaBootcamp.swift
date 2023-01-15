1bfdda9eaee1b02c036e478da5a28d0c485c9e49
woozoobro <woojusm@gmail.com>
2023-01-16 00:04:41 +0900

    SwiftUI

---
+++ SwiftUIBottcamp/BasicSwiftUI/BasicSwiftUI/SafeAreaBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  SafeAreaBootcamp.swift
+//  BasicSwiftUI
+//
+//  Created by 우주형 on 2023/01/15.
+//
+
+import SwiftUI
+
+struct SafeAreaBootcamp: View {
+    var body: some View {
+        ScrollView {
+            Text("Title goes here")
+                .font(.largeTitle)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            ForEach(0..<10) { index in
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(.white)
+                    .frame(height: 150)
+                    .shadow(radius: 10)
+                    .padding(20)
+            }
+        }
+        .background(
+            Color.green                
+        )
+    }
+}
+
+struct SafeAreaBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SafeAreaBootcamp()
+    }
+}
