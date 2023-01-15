fe0c3473ca3f5d87a8fd2d1980916896bb099b5e
woozoobro <woojusm@gmail.com>
2023-01-15 17:23:40 +0900

    SwiftUI

---
+++ SwiftUIBottcamp/BasicSwiftUI/BasicSwiftUI/StacksBootcamp.swift
@@ -0,0 +1,44 @@
+//
+//  StacksBootcamp.swift
+//  BasicSwiftUI
+//
+//  Created by 우주형 on 2023/01/15.
+//
+
+import SwiftUI
+
+struct StacksBootcamp: View {
+    // VStacks -> Vertical
+    // HStacks -> Horizontal
+    // ZStacks -> ZIndex (back to front)
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 200, height: 100)
+            Rectangle()
+                .fill(.orange)
+                .frame(width: 150, height: 100)
+            Rectangle()
+                .fill(.brown)
+                .frame(width: 100, height: 100)
+        }
+//        HStack {
+//            Rectangle()
+//                .fill(Color.green)
+//                .frame(width: 100, height: 100)
+//            Rectangle()
+//                .fill(.orange)
+//                .frame(width: 100, height: 100)
+//            Rectangle()
+//                .fill(.brown)
+//                .frame(width: 100, height: 100)
+//        }
+    }
+}
+
+struct StacksBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        StacksBootcamp()
+    }
+}
