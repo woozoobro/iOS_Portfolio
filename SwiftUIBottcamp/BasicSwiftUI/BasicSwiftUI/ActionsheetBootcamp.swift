737839f05e64bd88f52b41666d9e5e8a8e799ca8
woozoobro <woojusm@gmail.com>
2023-01-17 06:55:58 +0900

    SwiftUI

--- SwiftUIBottcamp/BasicSwiftUI/BasicSwiftUI/ActionsheetBootcamp.swift
+++ SwiftUIBottcamp/BasicSwiftUI/BasicSwiftUI/ActionsheetBootcamp.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct ActionsheetBootcamp: View {
+    
+    @State var showActionSheet: Bool = false
     var body: some View {
         Button("Click me") {
-            
+            showActionSheet.toggle()
         }
-    }
+        .confirmationDialog("Are you sure?", isPresented: $showActionSheet) {
+            Button("Delete all Items?", role: .destructive) {
+            }
+        } message: {
+            Text("You cannot undo this action")
+        }
+    }        
 }
 
 struct ActionsheetBootcamp_Previews: PreviewProvider {
