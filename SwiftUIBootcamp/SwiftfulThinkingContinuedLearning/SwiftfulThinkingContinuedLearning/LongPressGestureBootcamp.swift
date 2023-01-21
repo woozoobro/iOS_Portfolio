762ff7fad8b14e33b36de339c5f99e4dfbe21be6
woozoobro <woojusm@gmail.com>
2023-01-21 21:47:35 +0900

    swiftUI

--- SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/LongPressGestureBootcamp.swift
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/LongPressGestureBootcamp.swift
@@ -10,15 +10,55 @@ import SwiftUI
 struct LongPressGestureBootcamp: View {
     
     @State var isComplete: Bool = false
+    @State var isSuccess: Bool = false
     
     var body: some View {
         VStack {
             Rectangle()
-                .fill(.blue)
-                .frame(maxWidth: 100)
+                .fill(isSuccess ? Color.green : Color.blue)
+                .frame(maxWidth: isComplete ? .infinity : 0)
                 .frame(height: 55)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .background(.gray)
+            
+            HStack {
+                Text("Click Here")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) {
+                        //at the min duration
+                        withAnimation(.easeInOut) {
+                            isSuccess = true
+                        }
+                    } onPressingChanged: { isPressing in
+                        //start of Press -> min duration
+                        if isPressing {
+                            withAnimation(.easeInOut(duration: 1.0)) {
+                                isComplete = true
+                            }
+                        } else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                if !isSuccess {
+                                    withAnimation {
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                        }
+                    }
+                
+                Text("Reset")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        isComplete = false
+                        isSuccess = false
+                    }
+            }
         }
     }
 }
