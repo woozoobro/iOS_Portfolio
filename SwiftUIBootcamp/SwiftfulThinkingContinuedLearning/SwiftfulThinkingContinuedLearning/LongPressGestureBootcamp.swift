811207c05f6af5e3fb3bec2c6603a33768a6a70f
woozoobro <woojusm@gmail.com>
2023-01-21 20:13:24 +0900

    swiftui

---
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/LongPressGestureBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  LongPressGestureBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by 우주형 on 2023/01/21.
+//
+
+import SwiftUI
+
+struct LongPressGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LongPressGestureBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        LongPressGestureBootcamp()
+    }
+}
