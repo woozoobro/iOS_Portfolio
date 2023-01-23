ae7e1c8ac1c83ffd074c282e7ef9b739c0383358
woozoobro <woojusm@gmail.com>
2023-01-24 02:26:00 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/BasicSwiftUI/BasicSwiftUI/BackgroundMaterialsBootcamp.swift
@@ -0,0 +1,37 @@
+//
+//  BackgroundMaterialsBootcamp.swift
+//  BasicSwiftUI
+//
+//  Created by 우주형 on 2023/01/23.
+//
+
+import SwiftUI
+
+struct BackgroundMaterialsBootcamp: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            VStack {
+                RoundedRectangle(cornerRadius: 4)
+                    .frame(width: 30, height: 4)
+                    .padding()
+                Spacer()
+            }
+            .frame(height: 350)
+            .frame(maxWidth: .infinity)
+            .background(.ultraThinMaterial)
+            .cornerRadius(20)
+        }
+        .ignoresSafeArea()
+        .background(
+            Image("WooZoo")
+        )
+    }
+}
+
+struct BackgroundMaterialsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundMaterialsBootcamp()
+    }
+}
---
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/GeometryReaderBootcamp.swift
@@ -0,0 +1,39 @@
+//
+//  GeometryReaderBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by 우주형 on 2023/01/23.
+//
+
+import SwiftUI
+
+struct GeometryReaderBootcamp: View {
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        return Double(1 - (currentX / maxDistance))
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { index in
+                    GeometryReader { geomtry in
+                        RoundedRectangle(cornerRadius: 20)
+                            .rotation3DEffect(Angle(degrees: getPercentage(geo: geomtry) * 40),
+                                              axis: (x: 0.0, y: 1.0, z: 0.0))
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                }
+            }
+        }
+    }
+}
+
+struct GeometryReaderBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReaderBootcamp()
+    }
+}
