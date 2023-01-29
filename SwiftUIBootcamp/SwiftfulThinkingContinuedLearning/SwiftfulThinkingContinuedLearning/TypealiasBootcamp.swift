0cc4151705ca17dbb5178e8a736dd13313a17b25
woozoobro <woojusm@gmail.com>
2023-01-30 05:24:22 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/TypealiasBootcamp.swift
@@ -0,0 +1,35 @@
+//
+//  TypealiasBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by 우주형 on 2023/01/30.
+//
+
+import SwiftUI
+
+struct MoviewModel {
+    let title: String
+    let director: String
+    let count: Int
+}
+
+typealias TVModel = MoviewModel
+
+struct TypealiasBootcamp: View {
+    
+//    @State var item: MoviewModel = MoviewModel(title: "Title", director: "Joe", count: 5)
+    @State var item: TVModel = TVModel(title: "TV Title", director: "Emily", count: 10)
+    var body: some View {
+        VStack {
+            Text(item.title)
+            Text(item.director)
+            Text("\(item.count)")
+        }
+    }
+}
+
+struct TypealiasBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TypealiasBootcamp()
+    }
+}
