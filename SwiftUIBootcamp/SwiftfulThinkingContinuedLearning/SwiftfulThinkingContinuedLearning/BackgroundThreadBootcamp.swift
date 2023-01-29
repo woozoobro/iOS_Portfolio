c67d818bd494bd6dcc01bb0f7a952d38be7224eb
woozoobro <woojusm@gmail.com>
2023-01-29 20:38:34 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/BackgroundThreadBootcamp.swift
@@ -0,0 +1,69 @@
+//
+//  BackgroundThreadBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by 우주형 on 2023/01/29.
+//
+
+import SwiftUI
+
+class BackgroundThreadViewModel: ObservableObject {
+    @Published var dataArray: [String] = []
+    
+    func fetchData() {
+        
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+            
+            print("CHECK 1: \(Thread.isMainThread)")
+            print("CHECK 1: \(Thread.current)")
+            
+            DispatchQueue.main.async {
+                self.dataArray = newData
+                print("CHECK 2: \(Thread.isMainThread)")
+                print("CHECK 2: \(Thread.current)")
+            }
+        }
+    }
+    
+    private func downloadData() -> [String] {
+        var data: [String] = []
+        
+        for x in 0..<100 {
+            data.append("\(x)")
+            print(data)
+        }
+        return data
+    }
+}
+
+struct BackgroundThreadBootcamp: View {
+    
+    @StateObject var vm = BackgroundThreadViewModel()
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                Text("Load Data")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        vm.fetchData()
+                    }
+                
+                ForEach(vm.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundColor(.red)
+                    
+                }
+            }
+        }
+    }
+}
+
+struct BackgroundThreadBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundThreadBootcamp()
+    }
+}
