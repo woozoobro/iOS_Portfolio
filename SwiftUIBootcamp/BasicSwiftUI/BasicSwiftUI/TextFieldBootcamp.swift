d1a973d2a5ea0c67ae91e4a9495f189b00b6018c
woozoobro <woojusm@gmail.com>
2023-02-01 04:28:12 +0900

    SwiftUI

--- SwiftUIBootcamp/BasicSwiftUI/BasicSwiftUI/TextFieldBootcamp.swift
+++ SwiftUIBootcamp/BasicSwiftUI/BasicSwiftUI/TextFieldBootcamp.swift
@@ -61,8 +61,57 @@ import SwiftUI
 //}
 
 struct TextFieldBootcamp: View {
+    @State var textFieldText: String = ""
+    @State var dataArray: [String] = []
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                //.textFieldStyle(.roundedBorder)
+                    .padding()
+                    .background(Color.gray.opacity(0.3).cornerRadius(10))
+                    .foregroundColor(.red)
+                    .font(.headline)
+                
+                Button {
+                    if textIsAppropriate() {
+                        saveText()
+                    }
+                } label: {
+                    Text("Save".uppercased())
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(textIsAppropriate() ?  Color.blue : Color.gray)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                }
+                .disabled(!textIsAppropriate())
+                
+                ForEach(dataArray, id: \.self) { data in
+                    Text(data)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("TextField")
+        }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        // check text
+        if textFieldText.count >= 3 {
+            return true
+        }
+        
+        return false
+    }
+    
+    func saveText() {
+        dataArray.append(textFieldText)
+        textFieldText = ""
     }
 }
 
