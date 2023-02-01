f2e3b83e3b38852a23ef8a442be2c8a0fc6ecbe2
woozoobro <woojusm@gmail.com>
2023-02-02 07:45:19 +0900

    SwiftUI

--- SwiftUIBootcamp/BasicSwiftUI/BasicSwiftUI/StepperBootcamp.swift
+++ SwiftUIBootcamp/BasicSwiftUI/BasicSwiftUI/StepperBootcamp.swift
@@ -7,23 +7,54 @@
 
 import SwiftUI
 
+//struct StepperBootcamp: View {
+//    @State var stepperValue: Int = 10
+//    @State var widthIncrement: CGFloat = 0
+//    var body: some View {
+//        VStack {
+//            Stepper("Stepper: \(stepperValue)", value: $stepperValue)
+//                .padding(50)
+//            RoundedRectangle(cornerRadius: 25)
+//                .frame(width: 100 + widthIncrement, height: 100)
+//
+//            Stepper("Stepper 2") {
+//                // increment
+//                incrementWidth(amount: 30)
+//            } onDecrement: {
+//                // decrement
+//                incrementWidth(amount: -30)
+//            }
+//        }
+//    }
+//
+//    func incrementWidth(amount: CGFloat) {
+//        withAnimation(.easeInOut) {
+//            widthIncrement += amount
+//        }
+//    }
+//}
+
 struct StepperBootcamp: View {
+    
     @State var stepperValue: Int = 10
     @State var widthIncrement: CGFloat = 0
+    
     var body: some View {
         VStack {
             Stepper("Stepper: \(stepperValue)", value: $stepperValue)
                 .padding(50)
-            RoundedRectangle(cornerRadius: 25)
+            
+            RoundedRectangle(cornerRadius: 25.0)
                 .frame(width: 100 + widthIncrement, height: 100)
             
-            Stepper("Stepper 2") {
+            Stepper("Stepper2") {
                 // increment
-                incrementWidth(amount: 30)
+                incrementWidth(amount: 10)
             } onDecrement: {
                 // decrement
-                incrementWidth(amount: -30)
+                incrementWidth(amount: -10)
             }
+
         }
     }
     
