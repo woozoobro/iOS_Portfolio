98f149f97bd09a78f7864631137e0da93e365c77
woozoobro <woojusm@gmail.com>
2023-02-02 19:34:20 +0900

    SwiftUI

--- SwiftUIBootcamp/BasicSwiftUI/BasicSwiftUI/SliderBootcamp.swift
+++ SwiftUIBootcamp/BasicSwiftUI/BasicSwiftUI/SliderBootcamp.swift
@@ -7,28 +7,47 @@
 
 import SwiftUI
 
+//struct SliderBootcamp: View {
+//
+//    @State var sliderValue: Double = 3
+//    @State var color: Color = .red
+//    var body: some View {
+//        VStack {
+//            Text("Rating: \(String(format: "%.0f", sliderValue))")
+//                .foregroundColor(color)
+////            Slider(value: $sliderValue)
+////            Slider(value: $sliderValue, in: 1...5)
+////            Slider(value: $sliderValue, in: 1...5, step: 1.0)
+//            Slider(value: $sliderValue, in: 1...5, step: 1.0,
+//                   onEditingChanged: { _ in
+//                color = .green
+//            }, minimumValueLabel: Text("1"), maximumValueLabel: Text("5"),
+//                   label: {
+//                Text("Title")
+//            })
+//                .tint(.red)
+//        }
+//    }
+//}
+
 struct SliderBootcamp: View {
     
     @State var sliderValue: Double = 3
-    @State var color: Color = .red
+    @State var color: Color = .green
+    
     var body: some View {
         VStack {
-            Text("Rating: \(String(format: "%.0f", sliderValue))")
+            Text("Rating:")
+            Text(String(format: "%.0f", sliderValue))
                 .foregroundColor(color)
 //            Slider(value: $sliderValue)
 //            Slider(value: $sliderValue, in: 1...5)
-//            Slider(value: $sliderValue, in: 1...5, step: 1.0)
-            Slider(value: $sliderValue, in: 1...5, step: 1.0,
-                   onEditingChanged: { _ in
-                color = .green
-            }, minimumValueLabel: Text("1"), maximumValueLabel: Text("5"),
-                   label: {
-                Text("Title")
-            })
+            Slider(value: $sliderValue, in: 1...5, step: 1.0)
                 .tint(.red)
         }
-    }
+    }    
 }
+
 
 struct SliderBootcamp_Previews: PreviewProvider {
     static var previews: some View {
