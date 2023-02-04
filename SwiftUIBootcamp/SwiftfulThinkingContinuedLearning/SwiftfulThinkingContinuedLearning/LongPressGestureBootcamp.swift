eec3ae657971245cd2ac0882f8dd8fe604e91b7a
woozoobro <woojusm@gmail.com>
2023-02-04 17:56:56 +0900

    SwiftUI

--- SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/LongPressGestureBootcamp.swift
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/LongPressGestureBootcamp.swift
@@ -7,12 +7,67 @@
 
 import SwiftUI
 
+//struct LongPressGestureBootcamp: View {
+//
+//    @State var isComplete: Bool = false
+//    @State var isSuccess: Bool = false
+//
+//    var body: some View {
+//        VStack {
+//            Rectangle()
+//                .fill(isSuccess ? Color.green : Color.blue)
+//                .frame(maxWidth: isComplete ? .infinity : 0)
+//                .frame(height: 55)
+//                .frame(maxWidth: .infinity, alignment: .leading)
+//                .background(.gray)
+//
+//            HStack {
+//                Text("Click Here")
+//                    .foregroundColor(.white)
+//                    .padding()
+//                    .background(Color.black)
+//                    .cornerRadius(10)
+//                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) {
+//                        //at the min duration
+//                        withAnimation(.easeInOut) {
+//                            isSuccess = true
+//                        }
+//                    } onPressingChanged: { isPressing in
+//                        //start of Press -> min duration
+//                        if isPressing {
+//                            withAnimation(.easeInOut(duration: 1.0)) {
+//                                isComplete = true
+//                            }
+//                        } else {
+//                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+//                                if !isSuccess {
+//                                    withAnimation {
+//                                        isComplete = false
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//
+//                Text("Reset")
+//                    .foregroundColor(.white)
+//                    .padding()
+//                    .background(Color.black)
+//                    .cornerRadius(10)
+//                    .onTapGesture {
+//                        isComplete = false
+//                        isSuccess = false
+//                    }
+//            }
+//        }
+//    }
+//}
+
 struct LongPressGestureBootcamp: View {
-    
     @State var isComplete: Bool = false
     @State var isSuccess: Bool = false
-    
     var body: some View {
+        
         VStack {
             Rectangle()
                 .fill(isSuccess ? Color.green : Color.blue)
@@ -28,12 +83,12 @@ struct LongPressGestureBootcamp: View {
                     .background(Color.black)
                     .cornerRadius(10)
                     .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) {
-                        //at the min duration
+                        // at the min duration
                         withAnimation(.easeInOut) {
-                            isSuccess = true
+                            isSuccess.toggle()
                         }
                     } onPressingChanged: { isPressing in
-                        //start of Press -> min duration
+                        //start of press -> min duration
                         if isPressing {
                             withAnimation(.easeInOut(duration: 1.0)) {
                                 isComplete = true
@@ -41,7 +96,7 @@ struct LongPressGestureBootcamp: View {
                         } else {
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                                 if !isSuccess {
-                                    withAnimation {
+                                    withAnimation(.easeInOut) {
                                         isComplete = false
                                     }
                                 }
@@ -62,7 +117,6 @@ struct LongPressGestureBootcamp: View {
         }
     }
 }
-
 struct LongPressGestureBootcamp_Previews: PreviewProvider {
     static var previews: some View {
         LongPressGestureBootcamp()
