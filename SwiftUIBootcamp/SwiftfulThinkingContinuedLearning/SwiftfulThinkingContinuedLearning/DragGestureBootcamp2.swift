21dbc36a2cc2153d06ff6fb330f5e8255cf71bb6
woozoobro <woojusm@gmail.com>
2023-02-05 15:40:42 +0900

    SwiftUI

--- SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/DragGestureBootcamp2.swift
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/DragGestureBootcamp2.swift
@@ -7,6 +7,78 @@
 
 import SwiftUI
 
+//struct DragGestureBootcamp2: View {
+//
+//    @State var startingOffsetY: CGFloat = UIScreen.main.bounds.height * 0.85
+//    @State var currentDragOffsetY: CGFloat = 0
+//    @State var endingOffsetY: CGFloat = 0
+//
+//    var body: some View {
+//        ZStack {
+//            Color.green.ignoresSafeArea()
+//
+//            MySignUpView()
+//                .offset(y: startingOffsetY)
+//                .offset(y: currentDragOffsetY)
+//                .offset(y: endingOffsetY)
+//                .gesture(
+//                    DragGesture()
+//                        .onChanged { value in
+//                            withAnimation(.spring()) {
+//                                currentDragOffsetY = value.translation.height
+//                            }
+//                        }
+//                        .onEnded { value in
+//                            withAnimation(.spring()) {
+//                                if currentDragOffsetY < -150 {
+//                                    endingOffsetY = -startingOffsetY
+//                                } else if endingOffsetY != 0 && currentDragOffsetY > 150 {
+//                                    endingOffsetY = 0
+//                                }
+//                                currentDragOffsetY = 0
+//                            }
+//                        }
+//                )
+//            VStack {
+//                Text("start: \(startingOffsetY)")
+//                Text("current: \(currentDragOffsetY)")
+//                Text("end: \(endingOffsetY)")
+//            }
+//        }
+//        .ignoresSafeArea(edges: .bottom)
+//    }
+//}
+
+//struct MySignUpView: View {
+//    var body: some View {
+//        VStack(spacing: 20) {
+//            Image(systemName: "chevron.up")
+//                .padding(.top)
+//            Text("Sign up")
+//                .font(.headline)
+//                .fontWeight(.semibold)
+//
+//            Image(systemName: "flame.fill")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: 100, height: 100)
+//
+//            Text("This is the description for our app. This is my Favorite SwiftUI course and I recommend to all of my friends to subscribe to Swiftful Thinking")
+//                .multilineTextAlignment(.center)
+//
+//            Text("Create an account".uppercased())
+//                .foregroundColor(.white)
+//                .font(.headline)
+//                .padding()
+//                .padding(.horizontal)
+//                .background(Color.black.cornerRadius(10))
+//            Spacer()
+//        }
+//        .frame(maxWidth: .infinity)
+//        .background(Color.white).cornerRadius(30)
+//    }
+//}
+
 struct DragGestureBootcamp2: View {
     
     @State var startingOffsetY: CGFloat = UIScreen.main.bounds.height * 0.85
@@ -16,7 +88,6 @@ struct DragGestureBootcamp2: View {
     var body: some View {
         ZStack {
             Color.green.ignoresSafeArea()
-            
             MySignUpView()
                 .offset(y: startingOffsetY)
                 .offset(y: currentDragOffsetY)
@@ -39,13 +110,15 @@ struct DragGestureBootcamp2: View {
                             }
                         }
                 )
-            VStack {
-                Text("start: \(startingOffsetY)")
-                Text("current: \(currentDragOffsetY)")
-                Text("end: \(endingOffsetY)")
-            }
+            Text("\(currentDragOffsetY)")
         }
         .ignoresSafeArea(edges: .bottom)
+    }
+}
+
+struct DragGestureBootcamp2_Previews: PreviewProvider {
+    static var previews: some View {
+        DragGestureBootcamp2()
     }
 }
 
@@ -57,16 +130,14 @@ struct MySignUpView: View {
             Text("Sign up")
                 .font(.headline)
                 .fontWeight(.semibold)
-            
             Image(systemName: "flame.fill")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 100, height: 100)
             
-            Text("This is the description for our app. This is my Favorite SwiftUI course and I recommend to all of my friends to subscribe to Swiftful Thinking")
+            Text("This is the description for our app. this is my favorite SwiftUI course and I recommend to all of my friends to subscribe to Swiftful Thinking")
                 .multilineTextAlignment(.center)
-            
-            Text("Create an account".uppercased())
+            Text("Create an Account")
                 .foregroundColor(.white)
                 .font(.headline)
                 .padding()
@@ -75,12 +146,7 @@ struct MySignUpView: View {
             Spacer()
         }
         .frame(maxWidth: .infinity)
-        .background(Color.white).cornerRadius(30)
-    }
-}
-
-struct DragGestureBootcamp2_Previews: PreviewProvider {
-    static var previews: some View {
-        DragGestureBootcamp2()
+        .background(Color.white)
+        .cornerRadius(30)
     }
 }
--- SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/ScrollViewReaderBootcamp.swift
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/ScrollViewReaderBootcamp.swift
@@ -7,47 +7,64 @@
 
 import SwiftUI
 
+//struct ScrollViewReaderBootcamp: View {
+//
+//    @State var scrollToIndex: Int = 0
+//    @State var textFiledText: String = ""
+//
+//    var body: some View {
+//        VStack {
+//            TextField("Enter a # here...", text: $textFiledText)
+//                .frame(height: 55)
+//                .border(.gray)
+//                .padding(.horizontal)
+//                .keyboardType(.numberPad)
+//
+//            Button("Scroll Now") {
+//                withAnimation(.spring()) {
+//                    if let index = Int(textFiledText) {
+//                        scrollToIndex = index
+//                    }
+//                }
+//            }
+//
+//            ScrollView {
+//                ScrollViewReader { proxy in
+//
+//                    ForEach(0..<50) { index in
+//                        Text("This is item #\(index)")
+//                            .font(.headline)
+//                            .frame(height: 200)
+//                            .frame(maxWidth: .infinity)
+//                            .background(.white)
+//                            .cornerRadius(10)
+//                            .shadow(radius: 10)
+//                            .padding()
+//                            .id(index)
+//                    }
+//                    .onChange(of: scrollToIndex) { value in
+//                        withAnimation(.spring()) {
+//                            proxy.scrollTo(value, anchor: .top)
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
 struct ScrollViewReaderBootcamp: View {
-    
-    @State var scrollToIndex: Int = 0
-    @State var textFiledText: String = ""
-    
     var body: some View {
-        VStack {
-            TextField("Enter a # here...", text: $textFiledText)
-                .frame(height: 55)
-                .border(.gray)
-                .padding(.horizontal)
-                .keyboardType(.numberPad)
-            
-            Button("Scroll Now") {
-                withAnimation(.spring()) {
-                    if let index = Int(textFiledText) {
-                        scrollToIndex = index
-                    }
-                }
-            }
-            
-            ScrollView {
-                ScrollViewReader { proxy in
-                    
-                    ForEach(0..<50) { index in
-                        Text("This is item #\(index)")
-                            .font(.headline)
-                            .frame(height: 200)
-                            .frame(maxWidth: .infinity)
-                            .background(.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 10)
-                            .padding()
-                            .id(index)
-                    }
-                    .onChange(of: scrollToIndex) { value in
-                        withAnimation(.spring()) {
-                            proxy.scrollTo(value, anchor: .top)                            
-                        }
-                    }
-                }
+        ScrollView {
+            ForEach(0..<50) { index in
+                Text("This is item #\(index)")
+                    .font(.headline)
+                    .frame(height: 200)
+                    .frame(maxWidth: .infinity)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+                    .padding()
             }
         }
     }
