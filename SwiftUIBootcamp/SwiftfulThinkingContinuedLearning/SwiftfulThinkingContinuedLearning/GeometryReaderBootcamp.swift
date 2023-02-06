2a4deac459cb5f38f186a4af71067f4bb8967107
woozoobro <woojusm@gmail.com>
2023-02-06 19:31:47 +0900

    SwiftUI

--- SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/GeometryReaderBootcamp.swift
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/GeometryReaderBootcamp.swift
@@ -7,28 +7,52 @@
 
 import SwiftUI
 
+//struct GeometryReaderBootcamp: View {
+//
+//    func getPercentage(geo: GeometryProxy) -> Double {
+//        let maxDistance = UIScreen.main.bounds.width / 2
+//        let currentX = geo.frame(in: .global).midX
+//        return Double(1 - (currentX / maxDistance))
+//    }
+//
+//    var body: some View {
+//        ScrollView(.horizontal, showsIndicators: false) {
+//            HStack {
+//                ForEach(0..<20) { index in
+//                    GeometryReader { geomtry in
+//                        RoundedRectangle(cornerRadius: 20)
+//                            .rotation3DEffect(Angle(degrees: getPercentage(geo: geomtry) * 40),
+//                                              axis: (x: 0.0, y: 1.0, z: 0.0))
+//                    }
+//                    .frame(width: 300, height: 250)
+//                    .padding()
+//                }
+//            }
+//        }
+//    }
+//}
+
 struct GeometryReaderBootcamp: View {
-    
-    func getPercentage(geo: GeometryProxy) -> Double {
-        let maxDistance = UIScreen.main.bounds.width / 2
-        let currentX = geo.frame(in: .global).midX
-        return Double(1 - (currentX / maxDistance))
-    }
-    
+
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
+        ScrollView(.horizontal, showsIndicators: false ) {
             HStack {
                 ForEach(0..<20) { index in
-                    GeometryReader { geomtry in
+                    GeometryReader { geometry in
                         RoundedRectangle(cornerRadius: 20)
-                            .rotation3DEffect(Angle(degrees: getPercentage(geo: geomtry) * 40),
-                                              axis: (x: 0.0, y: 1.0, z: 0.0))
+                            .rotation3DEffect(Angle(degrees: getPercentage(geo: geometry) * 40), axis: (x: 0.0, y: 1.0, z: 0.0))
                     }
                     .frame(width: 300, height: 250)
                     .padding()
                 }
             }
         }
+    }
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        return Double(1 - (currentX / maxDistance))
     }
 }
 
