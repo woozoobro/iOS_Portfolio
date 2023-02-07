ab4dcd64ef1872eea635162ebc4d1c50d8bbece3
woozoobro <woojusm@gmail.com>
2023-02-07 20:37:50 +0900

    SwiftUI

--- SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/MaskBootcamp.swift
@@ -7,14 +7,55 @@
 
 import SwiftUI
 
+//struct MaskBootcamp: View {
+//
+//    @State var rating: Int = 2
+//
+//    var body: some View {
+//        ZStack {
+//            starsView
+//                .overlay(overlayView.mask(starsView))
+//        }
+//    }
+//
+//    private var overlayView: some View {
+//        GeometryReader { geometry in
+//            ZStack(alignment: .leading) {
+//                Rectangle()
+////                    .foregroundColor(.yellow)
+//                    .fill(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing))
+//                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+//            }
+//        }
+//        .allowsHitTesting(false)
+//    }
+//
+//    private var starsView: some View {
+//        HStack {
+//            ForEach(1..<6) { index in
+//                Image(systemName: "star.fill")
+//                    .font(.largeTitle)
+//                    .foregroundColor(Color.gray)
+//                    .onTapGesture {
+//                        withAnimation(.easeInOut(duration: 0.2)) {
+//                            rating = index
+//                        }
+//                    }
+//            }
+//        }
+//    }
+//}
+
 struct MaskBootcamp: View {
     
-    @State var rating: Int = 2
+    @State var rating: Int = 0
     
     var body: some View {
         ZStack {
             starsView
-                .overlay(overlayView.mask(starsView))
+                .overlay {
+                    overlayView.mask(starsView)
+                }
         }
     }
     
@@ -22,8 +63,8 @@ struct MaskBootcamp: View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
                 Rectangle()
-//                    .foregroundColor(.yellow)
-                    .fill(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing))
+                    .fill(LinearGradient(gradient: Gradient(colors: [.red, .blue, .yellow]), startPoint: .leading, endPoint: .trailing))
+                    .foregroundColor(.yellow)
                     .frame(width: CGFloat(rating) / 5 * geometry.size.width)
             }
         }
@@ -37,7 +78,7 @@ struct MaskBootcamp: View {
                     .font(.largeTitle)
                     .foregroundColor(Color.gray)
                     .onTapGesture {
-                        withAnimation(.easeInOut(duration: 0.2)) {
+                        withAnimation {
                             rating = index
                         }
                     }
