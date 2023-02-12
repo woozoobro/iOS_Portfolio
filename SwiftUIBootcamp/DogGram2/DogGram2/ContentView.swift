a951eac98d580eed7d5fcc62b7bb9d0e4aac5baf
woozoobro <woojusm@gmail.com>
2023-02-12 19:41:08 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/DogGram2/DogGram2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DogGram2
+//
+//  Created by 우주형 on 2023/02/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBootcamp/DogGram2/DogGram2/DogGram2App.swift
@@ -0,0 +1,17 @@
+//
+//  DogGram2App.swift
+//  DogGram2
+//
+//  Created by 우주형 on 2023/02/12.
+//
+
+import SwiftUI
+
+@main
+struct DogGram2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
