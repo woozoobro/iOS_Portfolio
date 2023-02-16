f6db1af515b39fc3b1483e8ceb85f6bd8129126a
woozoobro <woojusm@gmail.com>
2023-02-16 19:10:40 +0900

    SwiftUI

--- SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/CoreDataRelationshipsBootcamp.swift
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/CoreDataRelationshipsBootcamp.swift
@@ -314,7 +314,7 @@ class CoreDataManager {
     let context: NSManagedObjectContext
     
     init() {
-        container = NSPersistentContainer(name: "")
+        container = NSPersistentContainer(name: "CoreDataContainer")
         container.loadPersistentStores { description, error in
             if let error = error {
                 print("Error loading Core Data. \(error)")
@@ -326,6 +326,7 @@ class CoreDataManager {
     func save() {
         do {
             try context.save()
+            print("Saved successfully!")
         } catch let error {
             print("Error saving Core Data. \(error.localizedDescription)")
         }
@@ -337,8 +338,27 @@ class CoreDataRelationshipsViewModel: ObservableObject {
     let manager = CoreDataManager.instance
     @Published var businesses: [BusinessEntity] = []
     
+    init() {
+        getBusinesses()
+    }
+    
+    func getBusinesses() {
+        let request = NSFetchRequest<BusinessEntity>(entityName: "BusinessEntity")
+        do {
+            businesses =  try manager.context.fetch(request)
+        } catch let error {
+            print("Error fetching. \(error.localizedDescription)")
+        }
+    }
+    
     func addBusiness() {
         let newBusiness = BusinessEntity(context: manager.context)
+        newBusiness.name = "Apple"
+        save()
+    }
+    
+    func save() {
+        manager.save()
     }
     
 }
@@ -347,12 +367,60 @@ struct CoreDataRelationshipsBootcamp: View {
     @StateObject var vm = CoreDataRelationshipsViewModel()
     
     var body: some View {
-        Text("Hello")
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 20) {
+                    Button {
+                        vm.addBusiness()
+                    } label: {
+                        Text("Perform Action")
+                            .foregroundColor(.white)
+                            .frame(height: 55)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue).cornerRadius(10)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("Relationships")
+        }
     }
 }
 
 struct CoreDataRelationshipsBootcamp_Previews: PreviewProvider {
     static var previews: some View {
         CoreDataRelationshipsBootcamp()
+    }
+}
+
+struct BusinessView: View {
+    
+    let entity: BusinessEntity
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Name: \(entity.name ?? "")")
+                .bold()
+            if let departments = entity.departments?.allObjects as? [DepartmentEntity] {
+                Text("Departments:")
+                    .bold()
+                ForEach(departments) { department in
+                    Text(department.name ?? "")
+                }
+            }
+            
+            if let employees = entity.employees?.allObjects as? [EmployeeEntity] {
+                Text("Employees:")
+                    .bold()
+                ForEach(employees) { employee in
+                    Text(employee.name ?? "")
+                }
+            }
+        }
+        .padding()
+        .frame(maxWidth: 300, alignment: .leading)
+        .background(Color.gray.opacity(0.5))
+        .cornerRadius(10)
+        .shadow(radius: 10)
     }
 }
