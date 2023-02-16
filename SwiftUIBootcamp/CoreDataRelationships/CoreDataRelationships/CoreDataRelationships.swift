ea23a66b7a76dadd09cf7d84415c0d7999b4ed34
woozoobro <woojusm@gmail.com>
2023-02-16 23:53:05 +0900

    SwiftUI

--- SwiftUIBootcamp/CoreDataRelationships/CoreDataRelationships/CoreDataRelationships.swift
+++ SwiftUIBootcamp/CoreDataRelationships/CoreDataRelationships/CoreDataRelationships.swift
@@ -6,18 +6,199 @@
 //
 
 import SwiftUI
+import CoreData
 
-struct CoreDataRelationships: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//MARK: - CoreDataManager
+class CoreDataManager {
+    static let instance = CoreDataManager()
+    let container: NSPersistentContainer
+    let context: NSManagedObjectContext
+    
+    init() {
+        container = NSPersistentContainer(name: "CoreDataContainer")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Error loading Core Data. \(error)")
+            }
         }
-        .padding()
+        context = container.viewContext
+    }
+    
+    func save() {
+        do {
+            try context.save()
+            print("Saved Successfully!")
+        } catch let error {
+            print("Error saving Core Data. \(error.localizedDescription)")
+        }
     }
 }
+
+//MARK: - ViewModel
+class CoreDataRelationshipsViewModel: ObservableObject {
+    let manager = CoreDataManager.instance
+    @Published var businesses: [BusinessEntity] = []
+    @Published var departments: [DepartmentEntity] = []
+    
+    init() {
+        getBusiness()
+        getDepartments()
+    }
+    
+    func getBusiness() {
+        let request = NSFetchRequest<BusinessEntity>(entityName: "BusinessEntity")
+        do {
+            businesses = try manager.context.fetch(request)
+        } catch let error {
+            print("Error fetching. \(error.localizedDescription)")
+        }
+    }
+    
+    func getDepartments() {
+        let request = NSFetchRequest<DepartmentEntity>(entityName: "DepartmentEntity")
+        do {
+            departments = try manager.context.fetch(request)
+        } catch let error {
+            print("Error fetching. \(error.localizedDescription)")
+        }
+    }
+    
+    func addBusiness() {
+        let newBusiness = BusinessEntity(context: manager.context)
+        newBusiness.name = "Apple"
+        
+        // add existing departments to the new business
+        //newBusiness.departments = []
+        
+        // add existing employees to the new business
+        //newBusiness.employees = []
+        
+        // add new business to existing department
+        //newBusiness.addToDepartments(<#T##value: DepartmentEntity##DepartmentEntity#>)
+        
+        // add new business to existing employee
+        //newBusiness.addToEmployees(<#T##value: EmployeeEntity##EmployeeEntity#>)
+        
+        save()
+    }
+    
+    func addDepartment() {
+        let newDepartment = DepartmentEntity(context: manager.context)
+        newDepartment.name = "Marketing"
+        newDepartment.businesses = [businesses[0]]
+        save()
+    }
+    
+    func save() {
+        businesses.removeAll()
+        departments.removeAll()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.manager.save()
+            self.getBusiness()
+            self.getDepartments()
+        }
+    }
+}
+
+//MARK: - View
+struct CoreDataRelationships: View {
+    @State var vm = CoreDataRelationshipsViewModel()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 20) {
+                    Button {
+//                        vm.addBusiness()
+                        vm.addDepartment()
+                    } label: {
+                        Text("Perform Action")
+                            .foregroundColor(.white)
+                            .frame(height: 55)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue.cornerRadius(10))
+                    }
+                    
+                    ScrollView(.horizontal, showsIndicators: true) {
+                        HStack(alignment: .top) {
+                            ForEach(vm.businesses) { business in
+                                BusinessView(entity: business)
+                            }
+                        }
+                    }
+                    
+                }
+                .padding()
+            }
+            .navigationTitle("Relationships")
+        }
+    }
+}
+
+struct BusinessView: View {
+    let entity: BusinessEntity
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Name: \(entity.name ?? "")")
+                .bold()
+            
+            if let departments = entity.departments?.allObjects as? [DepartmentEntity] {
+                Text("Departments:")
+                    .bold()
+                ForEach(departments) { department in
+                    Text(department.name ?? "")
+                }
+            }
+            
+            if let employees = entity.employees?.allObjects as? [EmployeeEntity] {
+                Text("Employees:")
+                    .bold()
+                ForEach(employees) { employee in
+                    Text(employee.name ?? "")
+                }
+            }
+        }
+        .padding()
+        .frame(maxWidth: 300, alignment: .leading)
+        .background(Color.gray.opacity(0.5))
+        .cornerRadius(10)
+        .shadow(radius: 10)
+    }
+}
+
+struct DepartmentView: View {
+    let entity: DepartmentEntity
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Name: \(entity.name ?? "")")
+                .bold()
+            
+            if let businesses = entity.businesses?.allObjects as? [BusinessEntity] {
+                Text("Businesses:")
+                    .bold()
+                ForEach(businesses) { business in
+                    Text(business.name ?? "")
+                }
+            }
+            
+            if let employees = entity.employees?.allObjects as? [EmployeeEntity] {
+                Text("Employees:")
+                    .bold()
+                ForEach(employees) { employee in
+                    Text(employee.name ?? "")
+                }
+            }
+        }
+        .padding()
+        .frame(maxWidth: 300, alignment: .leading)
+        .background(Color.green.opacity(0.5))
+        .cornerRadius(10)
+        .shadow(radius: 10)
+    }
+}
+
 
 struct CoreDataRelationships_Previews: PreviewProvider {
     static var previews: some View {
