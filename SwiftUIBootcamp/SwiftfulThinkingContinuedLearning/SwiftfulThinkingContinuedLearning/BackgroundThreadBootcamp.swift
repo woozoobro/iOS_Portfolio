9dadfebd10504e90fad020d9195cfe873509b97e
woozoobro <woojusm@gmail.com>
2023-02-18 10:02:09 +0900

    SwiftUI

--- SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/BackgroundThreadBootcamp.swift
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/BackgroundThreadBootcamp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+/*
 class BackgroundThreadViewModel: ObservableObject {
     @Published var dataArray: [String] = []
     
@@ -56,6 +57,57 @@ struct BackgroundThreadBootcamp: View {
                         .font(.headline)
                         .foregroundColor(.red)
                     
+                }
+            }
+        }
+    }
+}
+*/
+
+class BackgroundThreadViewModel: ObservableObject {
+    @Published var dataArray: [String] = []
+    
+    func fetchData() {
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+            
+            print("Check1: \(Thread.isMainThread)")
+            print("Check1: \(Thread.current)")
+            
+            DispatchQueue.main.async {
+                self.dataArray = newData
+                print("Check2: \(Thread.isMainThread)")
+                print("Check2: \(Thread.current)")
+            }
+        }
+    }
+    
+    private func downloadData() -> [String] {
+        var data: [String] = []
+        for x in 0..<100 {
+            data.append("\(x)")
+            print(data)
+        }
+        return data
+    }
+}
+
+struct BackgroundThreadBootcamp: View {
+    @StateObject var vm = BackgroundThreadViewModel()
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                Text("Load Data")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        vm.fetchData()
+                    }
+                ForEach(vm.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundColor(.green)
                 }
             }
         }
