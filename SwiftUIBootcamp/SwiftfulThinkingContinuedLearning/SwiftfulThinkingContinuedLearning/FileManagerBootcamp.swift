7c31d5284831d67b96ca73de40e88bd34e600c66
woozoobro <woojusm@gmail.com>
2023-02-20 19:27:41 +0900

    SwiftUI

--- SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
@@ -7,9 +7,47 @@
 
 import SwiftUI
 
+class FileManagerViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    let imageName: String = "steve-jobs"
+    init() {
+        getImageFromAssetsFolder()
+    }
+    
+    func getImageFromAssetsFolder() {
+        image = UIImage(named: imageName)
+    }
+}
+
 struct FileManagerBootcamp: View {
+    
+    @StateObject var vm = FileManagerViewModel()
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                if let image = vm.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200,height: 200)
+                        .clipped()
+                        .cornerRadius(10)
+                }
+                
+                Button {
+                    
+                } label: {
+                    
+                }
+
+                
+                Spacer()
+                    
+            }
+            .navigationTitle("File Manager")
+        }
     }
 }
 
