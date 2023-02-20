90a409164058b5db17f9a6e4e7fec91f23307dc6
woozoobro <woojusm@gmail.com>
2023-02-20 19:13:28 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/SwiftfulThinkingContinuedLearning/SwiftfulThinkingContinuedLearning/FileManagerBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  FileManagerBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by 우주형 on 2023/02/20.
+//
+
+import SwiftUI
+
+struct FileManagerBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct FileManagerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        FileManagerBootcamp()
+    }
+}
