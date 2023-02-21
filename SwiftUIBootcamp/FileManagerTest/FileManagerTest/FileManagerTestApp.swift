950525eff521a8e37529a66fbd5402021de4f659
woozoobro <woojusm@gmail.com>
2023-02-21 20:10:17 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/FileManagerTest/FileManagerTest/FileManagerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerTestApp.swift
+//  FileManagerTest
+//
+//  Created by 우주형 on 2023/02/21.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FileManagerTest()
+        }
+    }
+}
