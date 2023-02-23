6ed5cf2d2e46086185479297be41944d4bd0a613
woozoobro <woojusm@gmail.com>
2023-02-23 23:46:07 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/SwiftfulCrypto/SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by 우주형 on 2023/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
