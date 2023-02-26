98c89d895444163af23865ad9a9e314927171db7
woozoobro <woojusm@gmail.com>
2023-02-26 20:40:28 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/SwiftfulCrypto/SwiftfulCrypto/Core/Components/SearchBarView.swift
@@ -0,0 +1,52 @@
+//
+//  SearchBarView.swift
+//  SwiftfulCrypto
+//
+//  Created by 우주형 on 2023/02/26.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    @Binding var searchText: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(
+                    searchText.isEmpty ? Color.theme.secondaryText : Color.theme.accent
+                )
+            
+            TextField("Search by name or symbol...", text: $searchText)
+                .foregroundColor(Color.theme.accent)
+                .autocorrectionDisabled(true)
+                .overlay(alignment: .trailing) {
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x: 10)
+                        .foregroundColor(Color.theme.accent)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                        .onTapGesture {
+                            UIApplication.shared.endEditing()
+                            searchText = ""
+                        }
+                }
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.theme.background)
+                .shadow(color: Color.theme.accent.opacity(0.15), radius: 10, x: 0, y: 0)
+        )
+        .padding()
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView(searchText: .constant(""))
+//            .preferredColorScheme(.dark)
+    }
+}
--- SwiftUIBootcamp/SwiftfulCrypto/SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUIBootcamp/SwiftfulCrypto/SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -12,6 +12,8 @@ class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
+    @Published var searchText: String = ""
+    
     private let dataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
     
@@ -20,7 +22,20 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscriber() {
-        dataService.$allCoins
+        
+        // updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .map { (text, startingCoins) -> [CoinModel] in
+                guard !text.isEmpty else { return startingCoins }
+                let lowercasedText = text.lowercased()
+                
+                return startingCoins.filter { (coin) -> Bool in
+                    return coin.name.lowercased().contains(lowercasedText) ||
+                    coin.symbol.lowercased().contains(lowercasedText) ||
+                    coin.id.lowercased().contains(lowercasedText)
+                }
+            }
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
