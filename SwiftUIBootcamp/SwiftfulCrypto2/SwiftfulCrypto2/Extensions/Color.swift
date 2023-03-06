ce0a5cf14a1d117ee2fce5d3b2cdcd99de73dd79
woozoobro <woojusm@gmail.com>
2023-03-06 11:47:18 +0900

    SwiftUI

--- SwiftUIBootcamp/SwiftfulCrypto2/SwiftfulCrypto2/Extensions/Color.swift
+++ SwiftUIBootcamp/SwiftfulCrypto2/SwiftfulCrypto2/Extensions/Color.swift
@@ -9,10 +9,8 @@ import Foundation
 import SwiftUI
 
 extension Color {
-    static let theme = ColorTheme()    
-    
+    static let theme = ColorTheme()
 }
-
 
 struct ColorTheme {
     let accent = Color("AccentColor")
