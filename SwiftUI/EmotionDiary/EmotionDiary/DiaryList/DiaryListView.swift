b7a3ef3a283ca2698bd215f4fc0b62d9c9109bc2
woozoobro <woojusm@gmail.com>
2023-03-14 22:40:20 +0900

    SwiftUI

---
+++ SwiftUI/EmotionDiary/EmotionDiary/DiaryList/DiaryListView.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryListView.swift
+//  EmotionDiary
+//
+//  Created by 우주형 on 2023/03/14.
+//
+
+import SwiftUI
+
+struct DiaryListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct DiaryListView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryListView()
+    }
+}
---
+++ SwiftUI/EmotionDiary/EmotionDiary/EmotionDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionDiaryApp.swift
+//  EmotionDiary
+//
+//  Created by 우주형 on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct EmotionDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DiaryListView()
+        }
+    }
+}
