f9648654e51545cbd3b87c46f75ef3b90da823a1
woozoobro <woojusm@gmail.com>
2023-03-20 22:17:49 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/DogGram2/DogGram2/Helpers/Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions.swift
+//  DogGram2
+//
+//  Created by 우주형 on 2023/03/20.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {    
+    struct MyTheme {
+        static var purpleColor: Color {
+            return Color("ColorPurple")
+        }
+        
+        static var yellowColor: Color {
+            return Color("ColorYellow")
+        }
+        
+        static var beigeColor: Color {
+            return Color("ColorBeige")
+        }
+    }
+}
--- SwiftUIBootcamp/DogGram2/DogGram2/Views/Screens/ContentView.swift
+++ SwiftUIBootcamp/DogGram2/DogGram2/Views/Screens/ContentView.swift
@@ -11,7 +11,30 @@ struct ContentView: View {
     var body: some View {
         TabView {
             Text("Screen 1")
+                .tabItem {
+                    Image(systemName: "book.fill")
+                    Text("Feed")
+                }
+            
+            Text("Screen 2")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Browse")
+                }
+            
+            Text("Screen 3")
+                .tabItem {
+                    Image(systemName: "square.and.arrow.up.fill")
+                    Text("Upload")
+                }
+            
+            Text("Screen 4")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
         }
+        .tint(Color.MyTheme.purpleColor)
     }
 }
 
