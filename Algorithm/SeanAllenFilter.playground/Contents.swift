c62b19e10bf480a3414e105e25044f8fb9f82b21
woozoobro <woojusm@gmail.com>
2023-03-22 19:20:35 +0900

    SwiftUI

--- Algorithm/SeanAllenFilter.playground/Contents.swift
+++ Algorithm/SeanAllenFilter.playground/Contents.swift
@@ -16,21 +16,42 @@ let appPortfolio: [IndieApp] = [
 // Filter
 
 //let freeApps = appPortfolio.filter { $0.monthlyPrice == 0 }
-//let highUsers = appPortfolio.filter { $0.users > 5000 }
-
-var highUsers: [IndieApp] = []
-
-for app in appPortfolio {
-    if app.users > 5000 {
-        highUsers.append(app)
-    }
-}
-
-print(highUsers)
+let highUsers = appPortfolio.filter { $0.users > 5000 }
+//print(highUsers)
 
 
 
 // Map
+let incresedPrices = appPortfolio.map { $0.monthlyPrice * 1.5 }
+//print(incresedPrices)
+
+
+// Reduce
+let numbers = [3,5,9,12,18]
+let sum = numbers.reduce(0, +)
+//print(sum)
+
+let totalUsers = appPortfolio.reduce(0, { $0 + $1.users})
+//print(totalUsers)
+
+
+// Chaining
+let recurringRevenue = appPortfolio.map { $0.monthlyPrice * Double($0.users) * 0.7 }.reduce(0, +)
+//print(recurringRevenue)
 
 
 
+// Compact Map
+let nilNumbers: [Int?] = [1, nil, 17, nil, 3, 7, nil, 99]
+let nonNilNumbers = nilNumbers.compactMap { $0 }
+//print(nonNilNumbers)
+
+
+
+// Flat Map
+let arrayOfArrays: [[Int]] = [[1,2,3],
+                              [4,5,6],
+                              [7,8,9]]
+
+let singleArray = arrayOfArrays.flatMap { $0.map { $0 * 2} }
+print(singleArray)
