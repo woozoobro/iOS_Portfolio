9e607d52532fd49a8ebbf6fa8102ab5237c9699c
woozoobro <woojusm@gmail.com>
2023-03-23 21:40:53 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/SwiftfulThinkingMaster/SwiftfulThinkingMaster/MatchedGeometryEffectBootcamp.swift
@@ -0,0 +1,73 @@
+//
+//  MatchedGeometryEffectBootcamp.swift
+//  SwiftfulThinkingMaster
+//
+//  Created by 우주형 on 2023/03/23.
+//
+
+import SwiftUI
+
+struct MatchedGeometryEffectBootcamp: View {
+    
+    @State private var isClicked: Bool = false
+    @Namespace private var namespace
+    
+    var body: some View {
+        VStack {
+            if !isClicked {
+                Circle()
+                    .matchedGeometryEffect(id: "rectangle", in: namespace)
+                    .frame(width: 100, height: 100)
+            }
+                        
+            Spacer()
+            if isClicked {
+                Circle()
+                    .matchedGeometryEffect(id: "rectangle", in: namespace)
+                    .frame(width: 300, height: 200)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.green)
+        .onTapGesture {
+            withAnimation(.easeInOut) {
+                isClicked.toggle()
+            }
+        }
+    }
+}
+
+struct MatchedGeometryEffectExample2: View {
+    
+    let categories: [String] = ["Home", "Popular", "Saved"]
+    @State private var selected: String = ""
+    @Namespace private var namespace2
+    
+    var body: some View {
+        HStack {
+            ForEach(categories, id: \.self) { category in
+                ZStack {
+                    if selected == category {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.red.opacity(0.5))
+                            .matchedGeometryEffect(id: "category_background", in: namespace2)
+                    }
+                    Text(category)
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: 55)
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        selected = category
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct MatchedGeometryEffectExample2_Previews: PreviewProvider {
+    static var previews: some View {
+        MatchedGeometryEffectExample2()
+    }
+}
