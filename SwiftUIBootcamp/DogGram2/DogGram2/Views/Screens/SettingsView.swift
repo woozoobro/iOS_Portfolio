519f7e2668264d77bf252d74c4ed23fb963b428a
woozoobro <woojusm@gmail.com>
2023-04-01 10:46:31 +0900

    DogGram2

---
+++ SwiftUIBootcamp/DogGram2/DogGram2/Views/Screens/SettingsView.swift
@@ -0,0 +1,84 @@
+//
+//  SettingsView.swift
+//  DogGram2
+//
+//  Created by 우주형 on 2023/04/01.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                //MARK: - Section 1: DogGram
+                GroupBox {
+                    HStack(spacing: 10) {
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                            .cornerRadius(12)
+                        Text("DogGram is the #1 app for posting pictures of your dog and sharing them across the world. We are a dog-loving community and we're happy to have you")
+                            .font(.footnote)
+                    }
+                } label: {
+                    SettingsLabelView(labelText: "DogGram", labelImage: "dot.radiowaves.left.and.right")
+                }
+                .padding()
+                
+                //MARK: - Section 2: Profile                
+                GroupBox {
+                    SettingsRowView(leftIcon: "pencil", text: "Display Name", color: Color.MyTheme.purpleColor)
+                    SettingsRowView(leftIcon: "text.quote", text: "Bio", color: Color.MyTheme.purpleColor)
+                    SettingsRowView(leftIcon: "photo", text: "Profile Picture", color: Color.MyTheme.purpleColor)
+                    SettingsRowView(leftIcon: "figure.walk", text: "Sign out", color: Color.MyTheme.purpleColor)
+                } label: {
+                    SettingsLabelView(labelText: "Profile", labelImage: "person.fill")
+                }
+                .padding()
+                
+                //MARK: - Section 3: Application
+                GroupBox {
+                    SettingsRowView(leftIcon: "folder.fill", text: "Privacy Policy", color: Color.MyTheme.yellowColor)
+                    SettingsRowView(leftIcon: "folder.fill", text: "Terms & Conditions", color: Color.MyTheme.yellowColor)
+                    SettingsRowView(leftIcon: "globe", text: "DogGram's Website", color: Color.MyTheme.yellowColor)
+                } label: {
+                    SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+                }
+                .padding()
+                
+                //MARK: - Section 4: Sign Off
+                
+                GroupBox {
+                    Text("DogGram was mad with love. \n All Rights Reserved \n Cool Apps Inc. \n Copyright 2020 ❤️")
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: .infinity)
+                }
+                .padding()
+                .padding(.bottom, 80)
+            }
+            .navigationTitle("Settings")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .font(.title)
+                    }
+                    .tint(.primary)
+                }
+            }
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
---
+++ SwiftUIBootcamp/DogGram2/DogGram2/Views/Subviews/SettingsRowView.swift
@@ -0,0 +1,41 @@
+//
+//  SettingsRowView.swift
+//  DogGram2
+//
+//  Created by 우주형 on 2023/04/01.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    var leftIcon: String
+    var text: String
+    var color: Color
+    
+    var body: some View {
+        HStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .fill(color)
+                Image(systemName: leftIcon)
+                    .font(.title3)
+                    .foregroundColor(.white)
+            }
+            .frame(width: 36, height: 36)
+            
+            
+            Text(text)
+            Spacer()
+            Image(systemName: "chevron.right")
+                .font(.headline)
+        }
+        .padding(.vertical, 4)
+    }
+}
+
+struct SettingsRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsRowView(leftIcon: "heart.fill", text: "Row Title", color: .red)
+            .previewLayout(.sizeThatFits)
+    }
+}
