6f15a1cafd02d678f71aa66a762471e69de293d7
woozoobro <woojusm@gmail.com>
2023-04-02 11:06:01 +0900

    SwiftUI

--- Algorithm/LeetCodeEasy.playground/Pages/TwoSum.xcplaygroundpage/Contents.swift
+++ Algorithm/LeetCodeEasy.playground/Pages/TwoSum.xcplaygroundpage/Contents.swift
@@ -1,13 +1,13 @@
 import Foundation
+//MARK: - Two sum Problem
 /*
 어떤 어레이가 주어지고 타겟이 주어지면
 어레이의 엘리먼트들을 더했을 때 합이 타겟값이 되는 엘리먼트의 index를 Array로 반환하는 Sum 만들기!
 
-```swift
 Input: nums [2, 7, 11, 15], target = 9
 Output: [0, 1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-```
+
  Input: nums = [3,2,4], target = 6
  Output: [1,2]
  
@@ -15,6 +15,9 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
  Output: [0,1]
 */
 
+
+//MARK: - My First Bruit force
+/*
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var a = 0
@@ -43,3 +46,31 @@ class Solution {
     }
 }
 
+*/
+
+//MARK: - The answer
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var dict = [Int: Int]()
+    for (index, num) in nums.enumerated() {
+        let complement = target - num
+        if let complementIndex = dict[complement] {
+            return [complementIndex, index]
+        }
+        dict[num] = index
+    }
+    return []
+}
+
+//func test() -> [Int : Int] {
+//    var numArray: [Int] = [5,23,55,6,8,11,36,78,90]
+//    var someDict = [Int : Int]()
+//
+//    for (index, num) in numArray.enumerated() {
+//
+//
+//        someDict[num] = index
+//        print(someDict)
+//    }
+//}
+//print(test())
--- SwiftUIBootcamp/DogGram2/DogGram2/Views/Screens/DogGram2App.swift
+++ SwiftUIBootcamp/DogGram2/DogGram2/Views/Screens/DogGram2App.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct DogGram2App: App {
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
+//MARK: - If using App Delegate
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//    func application(_ application: UIApplication,
+//                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        FirebaseApp.configure()
+//
+//        return true
+//    }
+//}
