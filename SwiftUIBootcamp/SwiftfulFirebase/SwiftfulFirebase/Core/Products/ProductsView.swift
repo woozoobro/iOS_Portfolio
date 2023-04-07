172a12fe41ba39b150d494dc51a77ba92dd4f2bf
woozoobro <woojusm@gmail.com>
2023-04-07 17:07:55 +0900

    Firebase

--- SwiftUIBootcamp/SwiftfulFirebase/SwiftfulFirebase/Core/Products/ProductsView.swift
+++ SwiftUIBootcamp/SwiftfulFirebase/SwiftfulFirebase/Core/Products/ProductsView.swift
@@ -10,15 +10,19 @@ import SwiftUI
 @MainActor
 final class ProductsViewModel: ObservableObject {
     
-    
     func downloadProductsAndUploadToFirebase() {
         // https://dummyjson.com/products
         guard let url = URL(string: "https://dummyjson.com/products") else { return }
         
         Task {
             do {
-                let (data, response) = try await URLSession.shared.data(from: url)
+                let (data, _) = try await URLSession.shared.data(from: url)
                 let products = try JSONDecoder().decode(ProductArray.self, from: data)
+                let productArray = products.products
+                
+                for product in productArray {
+                    try? await ProductsManager.shared.uploadProduct(product: product)
+                }
                 
                 print("SUCCESS")
                 print(products.products.count)
---
+++ SwiftUIBootcamp/SwiftfulFirebase/SwiftfulFirebase/Firestore/ProductsManager.swift
@@ -0,0 +1,25 @@
+//
+//  ProductsManager.swift
+//  SwiftfulFirebase
+//
+//  Created by 우주형 on 2023/04/07.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+final class ProductsManager {
+    
+    static let shared = ProductsManager()
+    private init() { }
+    
+    private let productsCollection = Firestore.firestore().collection("products")
+    private func productDocument(productId: String) -> DocumentReference {
+        productsCollection.document(productId)
+    }
+    
+    func uploadProduct(product: Product) async throws {
+        try productDocument(productId: String(product.id)).setData(from: product, merge: false)
+    }
+}
