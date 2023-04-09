153c2c023ee6a19642a3265f242e87ded29790b5
woozoobro <woojusm@gmail.com>
2023-04-10 00:58:13 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/SwiftfulThinkingMaster/SwiftfulThinkingMaster/AdvancedCombineBootcamp.swift
@@ -0,0 +1,80 @@
+//
+//  AdvancedCombineBootcamp.swift
+//  SwiftfulThinkingMaster
+//
+//  Created by 우주형 on 2023/04/10.
+//
+
+import SwiftUI
+import Combine
+
+class AdvancedCombineDataService {
+    //@Published var basicPublisher: String = "first publish"
+    let currentValuePublisher = CurrentValueSubject<String, Error>("first publish")
+    
+    init() {
+        publishFakeData()
+    }
+    
+    private func publishFakeData() {
+        let items = ["one", "two", "three"]
+        
+        for x in items.indices {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(x)) {
+                self.currentValuePublisher.send(items[x])
+            }
+        }
+    }
+}
+
+class AdvancedCombineBootcampViewModel: ObservableObject {
+    
+    @Published var data: [String] = []
+    let dataService = AdvancedCombineDataService()
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        dataService.currentValuePublisher
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Error: \(error.localizedDescription)")
+                    break
+                }
+            } receiveValue: { [weak self] returnedValue in
+                self?.data.append(returnedValue)
+            }
+            .store(in: &cancellables)
+    }
+    
+}
+
+struct AdvancedCombineBootcamp: View {
+    
+    @StateObject private var vm = AdvancedCombineBootcampViewModel()
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                ForEach(vm.data, id: \.self) {
+                    Text($0)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+            }
+        }
+    }
+}
+
+struct AdvancedCombineBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AdvancedCombineBootcamp()
+    }
+}
