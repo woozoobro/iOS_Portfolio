649c0c2b9ae51b3ad0c22ba3d801a363accd2251
woozoobro <woojusm@gmail.com>
2023-04-19 18:32:12 +0900

    SwiftUI

---
+++ SwiftUIBootcamp/BasicSwiftUI/BasicSwiftUI/TextSelectionBootcamp.swift
@@ -0,0 +1,21 @@
+//
+//  TextSelectionBootcamp.swift
+//  BasicSwiftUI
+//
+//  Created by 우주형 on 2023/04/19.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+struct TextSelectionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextSelectionBootcamp()
+    }
+}
