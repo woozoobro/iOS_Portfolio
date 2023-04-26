90afa5b802f6738596e9c05b5301f7032de9268f
woozoobro <woojusm@gmail.com>
2023-04-27 01:41:00 +0900

    Programmers

--- Algorithm/Programmers begin/ProgrammerBegin.swiftpm/ReplacingString.playground/Contents.swift
+++ Algorithm/Programmers begin/ProgrammerBegin.swiftpm/ReplacingString.playground/Contents.swift
@@ -1,5 +1,15 @@
 import UIKit
 
+/*
+ my_string, overwrite_string이 주어지고, 정수 s가 주어진다. 문자열 my_string의 s 인덱스부터 overwrite_string의 길이만큼
+ 문자열을 추가해버린 string을 반환하는 함수 작성하기
+ */
+
+
 func solution(_ my_string:String, _ overwrite_string:String, _ s:Int) -> String {
-    return ""
+    let prefix = my_string.prefix(s)
+    let suffix = my_string.suffix(from: my_string.index(my_string.startIndex, offsetBy: s))
+    
+    return prefix + overwrite_string + suffix
 }
+
---
+++ Algorithm/Programmers begin/ProgrammerBegin.swiftpm/Suggy.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// str1 = str2
+
+func solution(_ str1:String, _ str2:String) -> String {
+    var first = Array(str1)
+    var second = Array(str2)
+    first.map { firstItem -> String in
+        firstItem
+        return ""
+    }
+    
+    return ""
+}
