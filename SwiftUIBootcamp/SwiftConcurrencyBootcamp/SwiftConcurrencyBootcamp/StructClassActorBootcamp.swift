78d299270bb16471dfd169e934111401a9e1f7ec
woozoobro <woojusm@gmail.com>
2023-05-04 00:47:46 +0900

    StructClassActor

--- SwiftUIBootcamp/SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/StructClassActorBootcamp.swift
+++ SwiftUIBootcamp/SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/StructClassActorBootcamp.swift
@@ -11,21 +11,41 @@ struct StructClassActorBootcamp: View {
     var body: some View {
         Text("Hello, World!")
             .onAppear {
-                structTest1()
+                runTest()
+                
             }
     }
 }
 
-struct MyStruct {
+
+
+class MyClass {
     var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
 }
 
 extension StructClassActorBootcamp {
     private func runTest() {
         print("Test started!")
+//        structTest1()
+//        printDivider()
+//        classTest1()
+        structTest2()
+    }
+    
+    private func printDivider() {
+        print("""
+            
+            ----------------------------
+            
+            """)
     }
     
     private func structTest1() {
+        print("structTest1")
         let objectA = MyStruct(title: "Starting title!")
         print("ObjectA: ", objectA.title)
         
@@ -39,10 +59,76 @@ extension StructClassActorBootcamp {
         print("ObjectA: ", objectA.title)
         print("ObjectB: ", objectB.title)
     }
+    
+    private func classTest1() {
+        print("classTest1")
+        let objectA = MyClass(title: "Starting title!")
+        print("ObjectA: ", objectA.title)
+        
+        print("Pass the REFERENCE of objectA to objectB.")
+        let objectB = objectA
+        print("ObjectB: ", objectB.title)
+        
+        objectB.title = "Second title!"
+        print("ObjectB title changed.")
+        
+        print("ObjectA: ", objectA.title)
+        print("ObjectB: ", objectB.title)
+    }
 }
 
 struct StructClassActorBootcamp_Previews: PreviewProvider {
     static var previews: some View {
         StructClassActorBootcamp()
+    }
+}
+
+struct MyStruct {
+    var title: String
+}
+
+/// Immutable struct
+struct CustomStruct {
+    let title: String
+    
+    func updateTitle(newTitle: String) -> CustomStruct {
+        CustomStruct(title: newTitle)
+    }
+}
+
+struct MutatingStruct {
+    private(set) var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+    
+    mutating func updateTitle(newTitle: String) {
+        title = newTitle
+    }
+}
+
+extension StructClassActorBootcamp {
+    private func structTest2() {
+        print("structTest2")
+        var struct1 = MyStruct(title: "Title1")
+        print("Struct1: ", struct1.title)
+        struct1.title = "Title2"
+        print("Struct1: ", struct1.title)
+        
+        var struct2 = CustomStruct(title: "Title1")
+        print("Struct2: ", struct2.title)
+        struct2 = CustomStruct(title: "Title2")
+        print("Struct2: ", struct2.title)
+        
+        var struct3 = CustomStruct(title: "Title1")
+        print("Struct3: ", struct3.title)
+        struct3 = struct3.updateTitle(newTitle: "Title2")
+        print("Struct3: ", struct3.title)
+        
+        var struct4 = MutatingStruct(title: "Title1")
+        print("Struct4: ", struct4.title)
+        struct4.updateTitle(newTitle: "Title2")
+        print("Struct4: ", struct4.title)
     }
 }
