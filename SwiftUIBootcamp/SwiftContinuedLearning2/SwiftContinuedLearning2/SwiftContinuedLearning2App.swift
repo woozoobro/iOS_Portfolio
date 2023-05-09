9c526e1296063188339d69da561b03ed0ff399b8
woozoobro <woojusm@gmail.com>
2023-05-09 23:22:34 +0900

    SwiftUI

--- SwiftUIBootcamp/SwiftContinuedLearning2/SwiftContinuedLearning2/SwiftContinuedLearning2App.swift
+++ SwiftUIBootcamp/SwiftContinuedLearning2/SwiftContinuedLearning2/SwiftContinuedLearning2App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftContinuedLearning2App: App {
     var body: some Scene {
         WindowGroup {
-            LocalNotificationBootcamp()
+            DownloadWithCombine()
         }
     }
 }
