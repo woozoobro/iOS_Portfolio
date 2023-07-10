8c1de5f7dc124f2131d3f2a25b9b36f17f9d30d5
woozoobro <woojusm@gmail.com>
2023-07-10 21:57:26 +0900

    Scrumdinger

--- WhatIs/Scrumdinger/Scrumdinger/Views/MeetingView.swift
+++ WhatIs/Scrumdinger/Scrumdinger/Views/MeetingView.swift
@@ -21,8 +21,7 @@ struct MeetingView: View {
             VStack {
                 MeetingHeaderView(secondsElasped: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
                 
-                Circle()
-                    .strokeBorder(lineWidth: 24)
+                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme)
                 
                 MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
